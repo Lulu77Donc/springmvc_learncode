6707eb17f45e017cc7f18d5739996deb82e921b4
lijiaxiang_hela <1259465943@qq.com>
2025-03-14 19:31:34 +0800

    请求映射路径@RequestMapping

---
+++ springmvc-request-mapping/src/main/java/com/ljx/config/SpringMvcConfig.java
@@ -0,0 +1,9 @@
+package com.ljx.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ljx.controller")
+public class SpringMvcConfig {
+}
