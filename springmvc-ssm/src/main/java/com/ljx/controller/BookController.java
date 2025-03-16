e49cd2385a3aff05377f67cddf8fb19b9b3da4ed
lijiaxiang_hela <1259465943@qq.com>
2025-03-16 11:53:59 +0800

    ssm表现层和前端页面数据统一处理

--- springmvc-ssm/src/main/java/com/ljx/controller/BookController.java
+++ springmvc-ssm/src/main/java/com/ljx/controller/BookController.java
@@ -5,6 +5,7 @@ import com.ljx.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.CodeSource;
 import java.util.List;
 
 @RestController
@@ -15,34 +16,40 @@ public class BookController {
     private BookService bookService;
 
     @PostMapping
-    public boolean save(@RequestBody Book book) {
-        bookService.save(book);
-        return true;
+    public Result save(@RequestBody Book book) {
+        boolean flag = bookService.save(book);
+        return new Result(flag?Code.SAVE_OK:Code.SAVE_ERR,flag);
     }
 
 
     @PutMapping
-    public boolean update(@RequestBody Book book) {
-        bookService.update(book);
-        return true;
+    public Result update(@RequestBody Book book) {
+        boolean flag = bookService.update(book);
+        return new Result(flag?Code.UPDATE_OK:Code.UPDATE_ERR,flag);
     }
 
 
     @DeleteMapping("/{id}")
-    public boolean delete(@PathVariable Integer id) {
-        bookService.delete(id);
-        return true;
+    public Result delete(@PathVariable Integer id) {
+        boolean flag = bookService.delete(id);
+        return new Result(flag?Code.DELETE_OK:Code.DELETE_ERR,flag);
     }
 
 
     @GetMapping("/{id}")
-    public Book getById(@PathVariable Integer id) {
-        return bookService.getById(id);
+    public Result getById(@PathVariable Integer id) {
+        Book book = bookService.getById(id);
+        Integer code = book != null ? Code.GET_OK:Code.GET_ERR;
+        String msg = book != null ? "":"数据查询失败，请重试";
+        return new Result(code,book,msg);
     }
 
 
     @GetMapping
-    public List<Book> getAll() {
-        return bookService.getAll();
+    public Result getAll() {
+        List<Book> books = bookService.getAll();
+        Integer code = books != null ? Code.GET_OK:Code.GET_ERR;
+        String msg = books != null ? "":"数据查询失败，请重试";
+        return new Result(code,books,msg);
     }
 }
